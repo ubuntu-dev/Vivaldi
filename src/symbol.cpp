21338dd629825880b84ddf620ce6f25b1af51993
Jem Orgun <jeorgun@gmail.com>
2015-02-22 15:54:49 -0700

    Use STL hasher for symbol

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -23,6 +23,6 @@ const std::string& vv::to_string(symbol sym)
 
 size_t std::hash<vv::symbol>::operator()(const vv::symbol& sym) const
 {
-  // Use *simplest possible hash*
-  return reinterpret_cast<size_t>(sym.m_ptr);
+  const static std::hash<const std::string*> hasher{};
+  return hasher(sym.m_ptr);
 }
