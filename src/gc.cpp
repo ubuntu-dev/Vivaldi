b2fa8119e66c8668f959d1ebd080d1571e86c4f1
Jem Orgun <jeorgun@gmail.com>
2015-02-26 14:39:47 -0700

    Completely rework GC to use value types instead of pointers; ugly but much faster

--- src/gc.cpp
+++ src/gc.cpp
@@ -1,9 +1,8 @@
 #include "gc.h"
 
-#include "builtins.h"
-
 using namespace vv;
-
+using namespace gc;
+using namespace internal;
 
 value::nil gc::internal::g_nil{};
 value::boolean gc::internal::g_true{true};
@@ -12,43 +11,177 @@ std::array<value::integer, 1024> gc::internal::g_ints;
 
 namespace {
 
-std::vector<value::base*> g_vals;
+using gc_chunk = std::array<value_type, 512>;
+std::list<gc_chunk> g_vals( 4 );
+
+}
+
 vm::machine* g_vm;
 
-void mark()
+value_type& value_type::operator=(value::array&& other)
+{
+  (&base)->~base();
+  new (this) value::array{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::array_iterator&& other)
+{
+  (&base)->~base();
+  new (this) value::array_iterator{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::base&& other)
+{
+  (&base)->~base();
+  new (this) value::base{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::boolean&& other)
+{
+  (&base)->~base();
+  new (this) value::boolean{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::builtin_function&& other)
+{
+  (&base)->~base();
+  new (this) value::builtin_function{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::dictionary&& other)
+{
+  (&base)->~base();
+  new (this) value::dictionary{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::file&& other)
+{
+  (&base)->~base();
+  new (this) value::file{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::floating_point&& other)
+{
+  (&base)->~base();
+  new (this) value::floating_point{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::function&& other)
+{
+  (&base)->~base();
+  new (this) value::function{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::integer&& other)
+{
+  (&base)->~base();
+  new (this) value::integer{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::nil&& other)
+{
+  (&base)->~base();
+  new (this) value::nil{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::range&& other)
+{
+  (&base)->~base();
+  new (this) value::range{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::string&& other)
+{
+  (&base)->~base();
+  new (this) value::string{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::string_iterator&& other)
+{
+  (&base)->~base();
+  new (this) value::string_iterator{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::symbol&& other)
+{
+  (&base)->~base();
+  new (this) value::symbol{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(value::type&& other)
+{
+  (&base)->~base();
+  new (this) value::type{std::move(other)};
+  return *this;
+}
+value_type& value_type::operator=(vm::environment&& other)
+{
+  (&base)->~base();
+  new (this) vm::environment{std::move(other)};
+  return *this;
+}
+
+void get_next(std::list<gc_chunk>::iterator& major, value_type*& minor)
+{
+  while (major != end(g_vals)) {
+    minor = std::find_if(minor, end(*major),
+                         [](const auto& i)
+                           { return i.base.type == &builtin::type::nil; });
+    if (minor != end(*major))
+      break;
+    ++major;
+    minor = begin(*major);
+  }
+}
+
+value_type* gc::internal::get_next_empty()
+{
+  static auto major = begin(g_vals);
+  static auto minor = begin(*major);
+
+  if (major == end(g_vals)) {
+    mark();
+    sweep();
+    major = begin(g_vals);
+    minor = begin(*major);
+
+    get_next(major, minor);
+    if (major == end(g_vals)) {
+      --major;
+      g_vals.resize(g_vals.size() * 2);
+      ++major;
+      minor = begin(*major);
+    }
+  }
+
+  auto ret = minor;
+
+  // since minor is by definition empty at this point, *don't* include it in the
+  // next-empty search
+  ++minor;
+  get_next(major, minor);
+
+  assert(ret->base.type == &builtin::type::nil && "search failed\n");
+  return ret;
+}
+
+void gc::internal::mark()
 {
   if (g_vm)
     g_vm->mark();
 }
 
-void sweep()
+void gc::internal::sweep()
 {
-  for (auto*& i : g_vals) {
-    if (i->marked()) {
-      i->unmark();
-    } else {
-      delete i;
-      i = nullptr;
+  for (auto& i : g_vals) {
+    for (auto& j : i) {
+      if (j.marked()) {
+        j.unmark();
+      } else {
+        j = value::nil{};
+      }
     }
   }
-
-  g_vals.erase(remove(begin(g_vals), end(g_vals), nullptr), end(g_vals));
-}
-
-}
-
-value::base* gc::internal::emplace(value::base* val)
-{
-  if (g_vals.capacity() == g_vals.size()) {
-    mark();
-    sweep();
-    if (g_vals.capacity() == g_vals.size())
-      g_vals.reserve(g_vals.capacity() * 2);
-  }
-
-  val->unmark();
-  g_vals.push_back(val);
-  return val;
 }
 
 void gc::set_running_vm(vm::machine& vm)
@@ -61,12 +194,6 @@ void gc::init()
   int value = 0;
   for (auto& i : internal::g_ints)
     i.val = value++;
-  g_vals.reserve(512);
 }
 
-void gc::empty()
-{
-  for (auto i : g_vals)
-    delete i;
-  g_vals.clear();
-}
+void gc::empty() { }
--- src/gc.h
+++ src/gc.h
@@ -7,7 +7,24 @@
 #include "value/nil.h"
 #include "vm.h"
 
+#include "builtins.h"
+#include "value/array.h"
+#include "value/array_iterator.h"
+#include "value/builtin_function.h"
+#include "value/boolean.h"
+#include "value/dictionary.h"
+#include "value/integer.h"
+#include "value/file.h"
+#include "value/floating_point.h"
+#include "value/function.h"
+#include "value/nil.h"
+#include "value/range.h"
+#include "value/string.h"
+#include "value/string_iterator.h"
+#include "value/symbol.h"
+
 #include <array>
+#include <list>
 
 namespace vv {
 
@@ -15,7 +32,54 @@ namespace gc {
 
 namespace internal {
 
-value::base* emplace(value::base* item);
+union value_type {
+  value::array            array;
+  value::array_iterator   array_iterator;
+  value::base             base;
+  value::boolean          boolean;
+  value::builtin_function builtin_function;
+  value::dictionary       dictionary;
+  value::file             file;
+  value::floating_point   floating_point;
+  value::function         function;
+  value::integer          integer;
+  value::nil              nil;
+  value::range            range;
+  value::string           string;
+  value::string_iterator  string_iterator;
+  value::symbol           symbol;
+  value::type             type;
+
+  value_type& operator=(value::array&& array);
+  value_type& operator=(value::array_iterator&& array_iterator);
+  value_type& operator=(value::base&& base);
+  value_type& operator=(value::boolean&& boolean);
+  value_type& operator=(value::builtin_function&& builtin_function);
+  value_type& operator=(value::dictionary&& dictionary);
+  value_type& operator=(value::file&& file);
+  value_type& operator=(value::floating_point&& floating_point);
+  value_type& operator=(value::function&& function);
+  value_type& operator=(value::integer&& integer);
+  value_type& operator=(value::nil&& nil);
+  value_type& operator=(value::range&& range);
+  value_type& operator=(value::string&& string);
+  value_type& operator=(value::string_iterator&&  string_iterator);
+  value_type& operator=(value::symbol&& symbol);
+  value_type& operator=(value::type&& type);
+  value_type& operator=(vm::environment&& environment);
+
+  value_type() : nil{} { }
+
+  bool marked() const { return base.marked(); }
+  void unmark() { base.unmark(); }
+  void mark() { (&base)->mark(); }
+
+  bool empty() const { return base.type == &builtin::type::nil; }
+
+  operator value::base& () { return base; }
+
+  ~value_type() { (&base)->~base(); }
+};
 
 // Optimize common values
 extern value::nil g_nil;
@@ -23,12 +87,27 @@ extern value::boolean g_true;
 extern value::boolean g_false;
 extern std::array<value::integer, 1024> g_ints;
 
+void mark();
+void sweep();
+
+value_type* get_next_empty();
+
+template <typename T>
+inline value::base* emplace(T&& val)
+{
+  //auto first = try_emplace(val);
+  auto empty = get_next_empty();
+  *empty = std::move(val);
+  return &empty->base;
+}
+
 }
 
 template <typename T, typename... Args>
 inline T* alloc(Args&&... args)
 {
-  return static_cast<T*>(internal::emplace(new T{args...}));
+  //return static_cast<T*>(internal::emplace(new T{args...}));
+  return static_cast<T*>(internal::emplace(T{args...}));
 }
 
 // Optimized template overrides for alloc:
