6045bfbc9ee508a50301e29000847754334f2d9a
Jem Orgun <jeorgun@gmail.com>
2015-03-01 02:39:15 -0700

    Update comments in instruction.h

--- src/vm/instruction.h
+++ src/vm/instruction.h
@@ -26,46 +26,46 @@ struct type_t {
 };
 
 enum class instruction {
-  /// pushes the provided Bool literal into retval
+  /// pushes the provided Bool literal onto the stack
   pbool,
-  /// pushes the provided Float literal into retval
+  /// pushes the provided Float literal onto the stack
   pflt,
-  /// pushes the provided Function literal into retval
+  /// pushes the provided Function literal onto the stack
   pfn,
-  /// pushes the provided Integer literal into retval
+  /// pushes the provided Integer literal onto the stack
   pint,
-  /// pushes a Nil literal into retval
+  /// pushes a Nil literal onto the stack
   pnil,
-  /// pushes the provided String literal into retval
+  /// pushes the provided String literal onto the stack
   pstr,
-  /// pushes the provided Symbol literal into retval
+  /// pushes the provided Symbol literal onto the stack
   psym,
-  /// Pushes the provided Type literal into retval
+  /// Pushes the provided Type literal onto the stack
   ptype,
 
-  /// sets retval to an Array made out of the provided number of pushed args
+  /// pushes an Array made out of the provided number of pushed args
   parr,
-  /// sets retval to a Dictionary made out of the provided number of pushed args
+  /// pushes a Dictionary made out of the provided number of pushed args
   pdict,
 
-  /// reads a variable into retval
+  /// reads a variable onto the stack
   read,
-  /// writes retval to a variable
+  /// writes the top value to a variable
   write,
-  /// creates a new variable with value retval
+  /// creates a new variable with the value on top of the stack
   let,
 
-  /// reads self into retval
+  /// reads self onto the stack
   self,
   /// retrieves the nth passed argument, where n is the provided integer
   arg,
-  /// reads a member into retval
+  /// reads a member onto the stack
   readm,
-  /// sets a member to retval
+  /// pops the top off the stack, and pushes its member with the provided name
   writem,
-  /// calls retval, using the provided number of pushed arguments
+  /// calls the top of the stack, using the provided number of pushed arguments
   call,
-  /// creates new object of the type in retval
+  /// creates new object of the type on top of the stack
   pobj,
 
   /// pushes a (shallow) copy of the top of the stack onto the stack
@@ -86,15 +86,15 @@ enum class instruction {
 
   /// unconditionally jumps the provided number of commands
   jmp,
-  /// jump the provided number of commands if retval is falsy
+  /// jump the provided number of commands if the top value is falsy
   jf,
-  /// jump the provided number of commands if retval is trutyh
+  /// jump the provided number of commands if the top value is trutyh
   jt,
-  /// pushes retval as a new function for catching exceptions
+  /// pushes top value as a new function for catching exceptions
   pushc,
-  /// pops an exception catcher and discards it, leaving retval unchanged
+  /// pops an exception catcher and discards it, leaving top value unchanged
   popc,
-  /// throws retval as an exception
+  /// throws top value as an exception
   exc,
 
   /// Changes current directory to that passed as a string literal
